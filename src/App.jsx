e37015fe09c767b498292d6b8e0b28800e4fde8c
Sushma-M-Maddin <sushmamaddin@gmail.com>
2024-07-15 16:23:21 +0530

    Design header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Register from "./pages/Register";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="bg-slate-100">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-3">
+        <div className="flex items-center">
+          <img className="h-14 w-14" src={logo} alt="Logo" />
+          <Link to="/">
+            <h1 className="font-extrabold text-purple-700 ml-2 text-2xl">
+              EliteResume
+            </h1>
+          </Link>
+        </div>
+        <ul className="flex gap-16">
+          <Link to="/">
+            <li className="text-purple-700 font-bold font-serif">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="text-purple-700 font-bold font-serif">About</li>
+          </Link>
+          <Link to="/dashboard">
+            <li className="text-purple-700 font-bold font-serif">Dashboard</li>
+          </Link>
+          <Link to="/register">
+            <li className="text-purple-700 font-bold font-serif">Register</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
